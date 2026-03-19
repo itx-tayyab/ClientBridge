dd3f5bc0032b895271b8d17692796aa4273c4510
itx-tayyab <tayyabtariq838@gmail.com>
2026-03-19 22:03:17 +0500

    remove two routes

--- server/src/routes/messageRoutes.js
+++ server/src/routes/messageRoutes.js
@@ -1,15 +1,14 @@
 import { Router } from "express";
-import { sendMessage, getMessages, getConversations, getDirectMessages } from "../controllers/messagecontrollers.js";
+import { sendMessage, getMessages } from "../controllers/messagecontrollers.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
 const router = Router();
 
 router.post("/", authenticate, sendMessage);
 
-router.get("/conversations", authenticate, getConversations);
-
 router.get("/:projectId", authenticate, getMessages);
 
-router.get("/direct/:otherUserId", authenticate, getDirectMessages);
+//router.get("/conversations", authenticate, getConversations);
+//router.get("/direct/:otherUserId", authenticate, getDirectMessages);
 
 export default router;
