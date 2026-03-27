fda3dc29435b92b5f61ddf5e361002ffa7ab4948
itx-tayyab <tayyabtariq838@gmail.com>
2026-03-27 20:34:41 +0500

    project update and deletion routes added

--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { authenticate } from '../middlewares/authenticate.js';
-import { getallclients, createproject, getallprojects, getprojectdetails} from '../controllers/projectcontrollers.js';
-import { newMilestone, updateMilestoneStatus } from '../controllers/milestonecontrollers.js';
+import { getallclients, createproject, updateproject, getallprojects, getprojectdetails, deleteproject} from '../controllers/projectcontrollers.js';
+import { newMilestone, updateMilestoneStatus, updateMilestone, deleteMilestone } from '../controllers/milestonecontrollers.js';
 import { uploadFile } from '../controllers/filecontrollers.js';
 import { upload } from '../middlewares/upload.js';
 
@@ -11,13 +11,21 @@ router.get('/getclients', authenticate ,getallclients);
 
 router.post('/', authenticate, createproject);
 
+router.put('/:projectId', authenticate, updateproject);
+
 router.get('/getallprojects', authenticate, getallprojects);
 
 router.get('/:projectId', authenticate, getprojectdetails);
 
+router.delete('/:projectId', authenticate, deleteproject);
+
 router.post('/:projectId/newMilestone', authenticate, newMilestone);
 
+router.patch('/:projectId/milestone/:milestoneId', authenticate, updateMilestone);
+
 router.put('/:projectId/milestone/:milestoneId', authenticate, updateMilestoneStatus);
+    
+router.delete('/:projectId/milestone/:milestoneId', authenticate, deleteMilestone);
 
 router.post("/:projectId/files", authenticate, upload.single("file"), uploadFile);
 
