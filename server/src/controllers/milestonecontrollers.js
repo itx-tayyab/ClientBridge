099c625061ebda1c78beadd948d1228bde87ecef
itx-tayyab <tayyabtariq838@gmail.com>
2026-03-28 16:23:35 +0500

    update and delete logic for milestones

--- server/src/controllers/milestonecontrollers.js
+++ server/src/controllers/milestonecontrollers.js
@@ -37,6 +37,62 @@ export const newMilestone = async (req, res) => {
 
 }
 
+export const updateMilestone = async (req, res) => {
+
+    const { milestoneId} = req.params;
+    const { title, price, status, dueDate } = req.body;
+    
+    try {
+
+        const updatedMilestone = await prisma.milestone.update({
+            where: { id: Number(milestoneId) },
+            data: {
+                title,
+                price: price.toString(),
+                status,
+                dueDate: new Date(dueDate),
+            }
+        });
+
+        const progress = await calculateMilestoneProgress(Number(updatedMilestone.projectId));
+
+        res.status(200).json({
+            message: "Milestone updated successfully",
+            milestone: updatedMilestone,
+            projectProgress: progress,
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Updating Milestone Failed",
+            error: error.message,
+        })
+    }
+}
+
+export const deleteMilestone = async (req, res) => {
+
+    const { milestoneId } = req.params;
+
+    try {
+        const deletedMilestone = await prisma.milestone.delete({
+            where: { id: Number(milestoneId) },
+        });
+
+        const progress = await calculateMilestoneProgress(Number(deletedMilestone.projectId));
+
+        res.status(200).json({
+            message: "Milestone deleted successfully",
+            projectProgress: progress,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Deleting Milestone Failed",
+            error: error.message,
+        });
+    }
+}
+
 export const updateMilestoneStatus = async (req, res) => {
     try {
         const { projectId, milestoneId } = req.params;
@@ -88,10 +144,11 @@ const calculateMilestoneProgress = async (projectId) => {
         //console.log(`Completed milestones for project ${projectId}: ${completedMilestones}`);
 
         const progress = allmilestones === 0 ? 0 : Math.floor((completedMilestones / allmilestones) * 100);
+        const status = allmilestones > 0 && completedMilestones === allmilestones ? "COMPLETED" : "ACTIVE";
 
         await prisma.project.update({
             where: { id: Number(projectId) },
-            data: { progress },
+            data: { progress, status },
         });
         
         //console.log(`Project ${projectId} progress: ${progress}%`);
