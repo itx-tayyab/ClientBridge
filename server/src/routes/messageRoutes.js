8f2324b49090640a09642bc33b74e10996f1aa22
itx-tayyab <tayyabtariq838@gmail.com>
2026-03-28 16:24:41 +0500

    import function for global message

--- server/src/routes/messageRoutes.js
+++ server/src/routes/messageRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { sendMessage, getMessages } from "../controllers/messagecontrollers.js";
+import { sendMessage, getMessages, getConversations } from "../controllers/messagecontrollers.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
 const router = Router();
@@ -8,7 +8,6 @@ router.post("/", authenticate, sendMessage);
 
 router.get("/:projectId", authenticate, getMessages);
 
-//router.get("/conversations", authenticate, getConversations);
-//router.get("/direct/:otherUserId", authenticate, getDirectMessages);
+router.get("/conversations", authenticate, getConversations);
 
 export default router;
